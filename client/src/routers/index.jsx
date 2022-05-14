0e84b8f6b06b7b826ccceec58b8c29fc2f6fe0db
ledinhhuy189 <ledinhhuy.189@outlook.com.vn>
2022-05-14 10:35:12 +0700

    loader component for loading user data

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -1,45 +1,36 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Admin from '../features/Admin';
 import Auth from '../features/Auth';
-import { authData } from '../features/Auth/authSlice';
 import Home from '../features/Home';
 import Profile from '../features/Profile';
-import HomeSpinner from '../global/components/HomeSpinner';
 import NotFoundPage from '../global/pages/NotFoundPage';
+import Loader from '../layouts/Loader';
 import Main from '../layouts/Main';
 import Private from '../layouts/Private';
 
 const Routers = () => {
-   let location = useLocation();
-   const { loading } = useSelector(authData);
    return (
       <>
-         {loading ? (
-            <HomeSpinner />
-         ) : (
-            <>
-               {location.pathname === '/' && <Navigate to='/home' />}
-               <Routes>
-                  <Route path='/auth/*' element={<Auth />} />
-                  <Route element={<Main />}>
-                     <Route path='/home/*' element={<Home />} />
-                  </Route>
-                  <Route
-                     element={
-                        <Private>
-                           <Main />
-                        </Private>
-                     }
-                  >
-                     <Route path='/profile/*' element={<Profile />} />
-                     <Route path='/admin/*' element={<Admin />} />
-                  </Route>
-                  <Route path='*' element={<NotFoundPage />} />
-               </Routes>
-            </>
-         )}
+         <Loader>
+            <Routes>
+               <Route path='/auth/*' element={<Auth />} />
+               <Route element={<Main />}>
+                  <Route path='/home/*' element={<Home />} />
+               </Route>
+               <Route
+                  element={
+                     <Private>
+                        <Main />
+                     </Private>
+                  }
+               >
+                  <Route path='/profile/*' element={<Profile />} />
+                  <Route path='/admin/*' element={<Admin />} />
+               </Route>
+               <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+         </Loader>
       </>
    );
 };
